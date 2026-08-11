20aa7f9734e6545ddf557b7b72be36ed40f58565
Jimmy-ERC <jimmy.ramos@catolica.edu.sv>
2026-08-10 19:16:07 -0600

    Estructura inicial del proyecto ASP.NET

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lecturio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
